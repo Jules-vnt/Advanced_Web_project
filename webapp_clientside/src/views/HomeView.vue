093f5a619cd6f3c03dbaf8fa60f8bfd37d044194
Amadéo Berthelot <78102463+amadeo-dev@users.noreply.github.com>
2026-09-17 14:23:42 +0200

    router config

---
+++ webapp_clientside/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!-- TODO: put adama.jpg in src/assets/, then uncomment -->
+    <!-- <img alt="Vue logo" src="../assets/adama.jpg"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
